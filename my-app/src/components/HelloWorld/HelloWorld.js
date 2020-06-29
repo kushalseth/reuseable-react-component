c0de7322e9815631cd938ad3ec4509a03a767251
Kushal Seth <kushalseth1408@gmail.com>
2020-06-29 23:40:49 +0530

    props component

--- my-app/src/components/HelloWorld/HelloWorld.js
+++ my-app/src/components/HelloWorld/HelloWorld.js
@@ -1,13 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-/** A super lame component that says Hello with a custom message. */
+/* A super lame component that says Hello with a custom message.
+ *
+ */
 function HelloWorld({ message }) {
   return <div>Hello {message}</div>;
 }
 
 HelloWorld.propTypes = {
-  /** Message to display */
+  /** Message that we have to display */
   message: PropTypes.string,
 };
 
